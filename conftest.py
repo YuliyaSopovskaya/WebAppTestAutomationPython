fafa524aa99ed66e462edd1b13843b348f5388bf
YuliyaSopovskaya <109427956+YuliyaSopovskaya@users.noreply.github.com>
2023-08-28 16:45:31 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import requests
+import yaml
+
+with open('config.yaml') as file:
+my_dict=yaml.safe_load(file)
+
+url = my_dict['url']
+url1 = my_dict['url1']
+username = my_dict['username']
+password = my_dict['password']
+
+@pytest.fixture()
+def login():
+
+
+obj_data = requests.post(url=url, data ={'username':username, 'password':password})
+#print(obj_data.json()) #
+token = obj_data.json()['token']
+#print(token)
+return token
+
+#login()
